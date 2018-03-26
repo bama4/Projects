b8ba0683d8851bbdebe14cd39f09b6b3d0edb3dd
nallg00d <nick.allgood@gmail.com>
2018-03-25 21:37:17 -0400

    Removed extra files

--- PROJ1/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-import msg "./utils/message_defs"
-func main(){
-
-	var response = &msg.Message {Do: "something", Data: msg.Data{Key:"sample key"}}
-	fmt.Println(response.Data.Key)
-}
