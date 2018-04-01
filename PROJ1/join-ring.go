c4544e0f8c53a309410eaabd10f0df4f3e324199
nallg00d <nick.allgood@gmail.com>
2018-04-01 08:12:04 -0400

    Updated function

--- PROJ1/join-ring.go
+++ PROJ1/join-ring.go
@@ -5,15 +5,19 @@ import "strconv"
 import msg "./utils/message_defs"
 import chord "./utils/chord_defs"
 
-func join-ring(){
+func join-ring(sponsoring_node *ChordNode){
 	
 	// Check global map to see if ring exists
 	// if ring doesn't exist, create new ChordNode
-	// predecessor = nil
-	// successor = nil
+	if len(ring_nodes) == 0 {
+            &sponsoring_node.predecessor = nil
+            &sponsoring_node.successor = nil
+            ring_nodes[key] = sponsoring_node
+	}
+	else {
+	    &sponsoring_node.predecessor = nil
+	    &sponsoring_node.successor = find_successor()
+	}
 
-	// If global map has nodes
-	// Get sponsoring node from channel
-	// Call find-successor()
-
+	ring_nodes[key] = sponsoring_node
 }
