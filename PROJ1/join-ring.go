78a2c5357f953ad8e6a4fda8653650a65f02104b
nallg00d <nick.allgood@gmail.com>
2018-04-03 21:09:01 -0400

    cleaned up reference syntax, added explicit package declerations

--- PROJ1/join-ring.go
+++ PROJ1/join-ring.go
@@ -5,19 +5,19 @@ import "strconv"
 import msg "./utils/message_defs"
 import chord "./utils/chord_defs"
 
-func join-ring(sponsoring_node *ChordNode){
+func join-ring(sponsoring_node *chord.ChordNode){
 	
 	// Check global map to see if ring exists
 	// if ring doesn't exist, create new ChordNode
-	if len(ring_nodes) == 0 {
-            &sponsoring_node.predecessor = nil
-            &sponsoring_node.successor = nil
-            ring_nodes[key] = sponsoring_node
+	if len(chord.ring_nodes) == 0 {
+            sponsoring_node.predecessor = nil
+            sponsoring_node.successor = nil
+            chord.ring_nodes[key] = sponsoring_node
 	}
 	else {
-	    &sponsoring_node.predecessor = nil
-	    &sponsoring_node.successor = find_successor()
+	    sponsoring_node.predecessor = nil
+	    sponsoring_node.successor = find_successor()
 	}
 
-	ring_nodes[key] = sponsoring_node
+	chord.ring_nodes[key] = sponsoring_node
 }
