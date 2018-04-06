c1d428ce72aa8e949da5f6acce29a660c2c4ee87
nallg00d <nick.allgood@gmail.com>
2018-04-05 20:20:05 -0400

    Updated definitions to new structure

--- PROJ1/leave-ring.go
+++ PROJ1/leave-ring.go
@@ -3,22 +3,23 @@ package leave-ring
 import "fmt"
 import "strconv"
 import msg "./utils/message_defs"
-import chord "./utils/chord_defs"
+import chord "./utils/node_defs"
 
-func leave-ring(sponsoring_node *chord.ChordNode, mode String) {
+func leave-ring(node_id int, sponsoring_node *chord.Node, mode String) {
 
 	// Leaves orderly or immediate
 
 	switch mode { 
 	// Immediate just removes node, doesn't tell neighbors anything
 	case "immediate":
-		sponsoring_node.predecessor = nil
-		sponsoring_node.successor = nil
+		sponsoring_node.Predecessor = nil
+		sponsoring_node.Successor = nil
+	// Orderly requires dumping messages to successor before leaving
 	case "orderly:
 		// stuff to tell otehr nodes 
 		// stuff to dump data to other nodes
-		sponsoring_node.predecessor = nil
-		sponsoring_node.successor = nil
+		sponsoring_node.Predecessor = nil
+		sponsoring_node.Successor = nil
 	default:
 		fmt.Println("Error!")
 	}
