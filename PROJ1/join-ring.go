9676037e75480161f0d512a15d284f2e47f550a0
nallg00d <nick.allgood@gmail.com>
2018-04-05 20:14:43 -0400

    Updated defs based on new node stuff

--- PROJ1/join-ring.go
+++ PROJ1/join-ring.go
@@ -3,20 +3,20 @@ package join-ring
 import "fmt"
 import "strconv"
 import msg "./utils/message_defs"
-import chord "./utils/chord_defs"
+import chord "./utils/node_defs"
 
-func join-ring(sponsoring_node *chord.ChordNode){
+func join-ring(sponsoring_node *chord.Node){
 	
 	// Check global map to see if ring exists
 	// if ring doesn't exist, create new ChordNode
 	if len(chord.ring_nodes) == 0 {
-            sponsoring_node.predecessor = nil
-            sponsoring_node.successor = nil
+            sponsoring_node.Predecessor = nil
+            sponsoring_node.Successor = nil
             chord.ring_nodes[key] = sponsoring_node
 	}
 	else {
-	    sponsoring_node.predecessor = nil
-	    sponsoring_node.successor = find_successor()
+	    sponsoring_node.Predecessor = nil
+	    sponsoring_node.Successor = find_successor()
 	}
 
 	chord.ring_nodes[key] = sponsoring_node
