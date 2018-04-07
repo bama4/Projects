4e9f6453029fdc2e2712abfb2bc4b8f717480fc9
nallg00d <nick.allgood@gmail.com>
2018-04-07 17:31:02 -0400

    Fixup join ring, removed ring_node references

--- PROJ1/join_ring/join_ring.go
+++ PROJ1/join_ring/join_ring.go
@@ -1,25 +1,15 @@
 package join_ring
 
-import "fmt"
-import "strconv"
-import msg "../utils/message_defs"
+//import "fmt"
+//import "strconv"
+//import msg "../utils/message_defs"
 import chord "../utils/node_defs"
 
 // Gets sponsoring node ID to lookup
 // Node object is the node that wants to join
-func join-ring(sponsoring_node_id int, node *chord.Node){
+func join_ring(sponsoring_node_id int, node *chord.Node){
 	
-	// Check global map to see if ring exists
-	if ring_nodes == nil {
-            node.Predecessor = nil
-            node.Successor = nil
-           ring_nodes[0] = node
-	}
-	// Add check to see if node id is in map/chord ring
-	else {
-	    node.Predecessor = nil
-	    //node.Successor = find_successor(sponsoring_node_id)
-	}
+    node.Predecessor = nil
+    //node.Successor = find_successor(sponsoring_node_id)
 
-	//ring_nodes[key] = node
 }
