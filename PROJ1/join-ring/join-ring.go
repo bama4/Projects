2c2370c236ccb7361bd233d862a195adbb1d5fda
nallg00d <nick.allgood@gmail.com>
2018-04-07 17:15:34 -0400

    Added join-ring subdir for package

--- PROJ1/join-ring/join-ring.go
+++ PROJ1/join-ring/join-ring.go
@@ -5,20 +5,22 @@ import "strconv"
 import msg "./utils/message_defs"
 import chord "./utils/node_defs"
 
-func join-ring(node_id int, node *chord.Node){
+// Gets sponsoring node ID to lookup
+// Node object is the node that wants to join
+func join-ring(sponsoring_node_id int, node *chord.Node){
 	
 	// Check global map to see if ring exists
 	// if ring doesn't exist, create new ChordNode
 	if ring_nodes == nil {
             node.Predecessor = nil
             node.Successor = nil
-            ring_nodes[key] = node
+           ring_nodes[0] = node
 	}
 	// Add check to see if node id is in map/chord ring
 	else {
 	    node.Predecessor = nil
-	    //node.Successor = find_successor()
+	    //node.Successor = find_successor(sponsoring_node_id)
 	}
 
-	ring_nodes[key] = node
+	//ring_nodes[key] = node
 }
