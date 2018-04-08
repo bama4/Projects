3bf0506e74a4e4e4095beb2efdf1accae92e2373
nallg00d <nick.allgood@gmail.com>
2018-04-08 13:36:08 -0400

    Fixed Go annoyance with capital letter for func, also modified param to int64

--- PROJ1/join_ring/join_ring.go
+++ PROJ1/join_ring/join_ring.go
@@ -7,7 +7,7 @@ import chord "../utils/node_defs"
 
 // Gets sponsoring node ID to lookup
 // Node object is the node that wants to join
-func join_ring(sponsoring_node_id int, node *chord.Node){
+func Join_ring(sponsoring_node_id int64, node *chord.Node){
 	
     node.Predecessor = nil
     //node.Successor = find_successor(sponsoring_node_id)
