5d9045a41a4c022d33da9db9e2d0acc31c245db2
nallg00d <nick.allgood@gmail.com>
2018-04-13 23:07:19 -0400

    Started on put

--- PROJ1/put/put.go
+++ PROJ1/put/put.go
@@ -1,10 +1,16 @@
 import "fmt"
+import msg "./utils/message_defs"
+import node "./utils/node_defs"
 
 
+//{"do": "put", "data": { "key" : "a key", "value" : "a value" }, "respond-to": "channel-id"}
+// instructing the receipient node to store the given (key,value) pair in the appropriate ring node. 
+// Node gets this message
 
+func Put(data *msg.Data, respond_to int64) {
 
-
-func put() {
+	// Node gets data 
+	node.Data = data
 
 
 }
