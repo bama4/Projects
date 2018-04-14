f2987c9c051bc61ff68f78a00f70a4dc045ac19a
mohitkhatwani <khatwani13@gmail.com>
2018-04-14 17:15:49 -0400

    Changed iteration of fingerTable and func arguments

--- PROJ1/findSuccessor/findSuccessor.go
+++ PROJ1/findSuccessor/findSuccessor.go
@@ -1,25 +1,23 @@
 package findSuccessor
 
 import (
-	"math"
-
 	nodeDefs "../utils/node_defs"
 )
 
 //FindSuccessor ... This function is used to find successor of node with ID targetID
-func FindSuccessor(sponsoringNode *nodeDefs.Node, targetID int64, totalNodes int) (successor *nodeDefs.Node) {
+func FindSuccessor(sponsoringNode *nodeDefs.Node, targetID int64) (successor *nodeDefs.Node) {
 	if targetID > sponsoringNode.ChannelId && targetID <= sponsoringNode.Successor.ChannelId {
 		successor = sponsoringNode.Successor
 	} else {
-		precedingNode := findClosestPrecedingNode(sponsoringNode, targetID, totalNodes)
-		successor = FindSuccessor(precedingNode, targetID, totalNodes)
+		precedingNode := findClosestPrecedingNode(sponsoringNode, targetID)
+		successor = FindSuccessor(precedingNode, targetID)
 	}
 	return successor
 }
 
-func findClosestPrecedingNode(sponsoringNode *nodeDefs.Node, targetID int64, totalNodes int) (precedingNode *nodeDefs.Node) {
+func findClosestPrecedingNode(sponsoringNode *nodeDefs.Node, targetID int64) (precedingNode *nodeDefs.Node) {
 
-	for i := int(math.Log2(float64(totalNodes))) - 1; i >= 0; i-- {
+	for i := range sponsoringNode.FingerTable {
 		if sponsoringNode.FingerTable[int64(i)].ChannelId > sponsoringNode.ChannelId && sponsoringNode.FingerTable[int64(i)].ChannelId < targetID {
 			precedingNode = sponsoringNode.FingerTable[int64(i)]
 		}
