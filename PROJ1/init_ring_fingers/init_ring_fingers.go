940cd8b0d8f67b15f7a9c0395fe010384c565350
Mariama Adangwa <bama4@umbc.edu>
2018-04-15 18:05:24 -0400

    Changed fingertable to be size N and number of nodes 2^N; first nodes fingertable is initialized to contain all entries with references to n

--- PROJ1/init_ring_fingers/init_ring_fingers.go
+++ PROJ1/init_ring_fingers/init_ring_fingers.go
@@ -7,12 +7,12 @@ import "sync"
 */
 var map_lock = sync.Mutex{}
 
-func Init_Ring_FingerTable(node *chord.Node){
+func Init_Ring_FingerTable(node *chord.Node, number_of_network_nodes int){
 
 	if len(node.FingerTable) == 0	{
 		//The finger tables are m bits long where m is the number of bits in the identifier.
-		for i:=0; i < 64; i++ {
-			//Should have 64 entries that increment in i + 2^i
+		for i:=0; i < number_of_network_nodes; i++ {
+			//Should have N entries for a ring or up to 2^N nodes
 			map_lock.Lock()
 			node.FingerTable[int64(i)] = nil
 			map_lock.Unlock()
