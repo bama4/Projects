40da8c0e16f0be6f92de8e5dbc18348d67a90a03
nallg00d <nick.allgood@gmail.com>
2018-04-15 11:21:34 -0400

    Added better code to look in node finger table

--- PROJ1/put/put.go
+++ PROJ1/put/put.go
@@ -1,39 +1,68 @@
 import "fmt"
 import msg "./utils/message_defs"
 import node "./utils/node_defs"
+import "strconv"
 
 
 //{"do": "put", "data": { "key" : "a key", "value" : "a value" }, "respond-to": "channel-id"}
 // instructing the receipient node to store the given (key,value) pair in the appropriate ring node. 
+
 // Node gets this message
+// Use all the nodes fields
+
+func find_biggest(data *msg.Data, node_id int64)(biggest int64) {
+
+	var bigger := 0
+	var old_k := 0
+	
+	for k, v := range FingerTable {
+		if int64(k) < node_id {
+			bigger := k
+
+		}	
+	}
+	
+	return
+}
 
 func Put(data *msg.Data, respond_to int64) {
+
+	// Biggest keyvalu
+	var biggest = 0
 
 	// Get the node ID for data string
 	var node_id = map_to_string_id(data.Key)
 
-	// Node gets data 
+	// Look in current node (this) fingertable	
+	for k, v := range FingerTable {
+		if k == node_id {
+			// We have a direct mapping for the key, go to this node
+
+		} else {
+
+			// We don't have a direct mapping so we need to go to the next node in the finger table
+			// We go to the node that is closest without over shooting
+			if k < node_id {
+				biggest = k
+
+
+			}
+
+			
+
+
+		}		
+
+	}
+
 	// DataTable is also a map
 //	ring_nodes[node_id].DataTable = data.Value
 	// Need to do a lookup to a node and route to finger tables
 
-	for i := 0; i < len(ring_nodes); i++ {
-		if ring_nodes[i] != node_id {
-			// Look in finger table
-			for j :=0; j < len(ring_nodes[i].FingerTable); j++ {
-				
-
-
-			}	
-
-		}	
-
-	}
-
 
 	//ring_nodes[node_id].Value = data.Value
 	
-	// network[respond_to] <- "Put"
+	network[respond_to] <- "Putting data at node xx"
 
 
 }
