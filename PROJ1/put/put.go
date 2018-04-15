a01f5f25028e7731424d7265077364b7f95a3647
nallg00d <nick.allgood@gmail.com>
2018-04-14 23:14:54 -0400

    Attempting to setup code to go through chord ring

--- PROJ1/put/put.go
+++ PROJ1/put/put.go
@@ -14,7 +14,21 @@ func Put(data *msg.Data, respond_to int64) {
 
 	// Node gets data 
 	// DataTable is also a map
-	ring_nodes[node_id].DataTable = data.Value
+//	ring_nodes[node_id].DataTable = data.Value
+	// Need to do a lookup to a node and route to finger tables
+
+	for i := 0; i < len(ring_nodes); i++ {
+		if ring_nodes[i] != node_id {
+			// Look in finger table
+			for j :=0; j < len(ring_nodes[i].FingerTable); j++ {
+				
+
+
+			}	
+
+		}	
+
+	}
 
 
 	//ring_nodes[node_id].Value = data.Value
