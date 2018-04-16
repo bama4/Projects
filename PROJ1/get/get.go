8a6e593597f9fa61448dd0cff3e33e0b115b96be
nallg00d <nick.allgood@gmail.com>
2018-04-15 22:41:34 -0400

    Fixed/cleaned up Get similar to Put

--- PROJ1/get/get.go
+++ PROJ1/get/get.go
@@ -7,27 +7,31 @@ import node "./utils/node_defs"
 
 func Get(key String, respond-to int64)(value string) {
 
+	var found = false
+	var closest_node = nil
 	// Get Node Data
 	var node_id = map_to_string_id(key)
 
 	// Check to see if we have it
 	if ChannelID == node_id {
-
 		value := DataTable[key]
-
-	}
-
-	// Lookup key in own finger table
-	for k, v := range FingerTable {
-		if k == node_id {
-			value := DataTable[key]
-			return
-		} else {
-
-			var closest_node = find_biggest_node(FingerTable, node_id)
-			value := closest_node.DataTable[key]
-			return	
+	} else {
+	
+		// Check out Finger tables
+		for k, v := range FingerTable {
+			if k == node_id {
+				value = DataTable[node_id]
+				found = true
+				break
+			}
 		}
+	}
+	
+	// It's not in our finger table so we need to find the next highest node
+	// without over shooting
+	if found == false {
+		closest_node = FindClosestPreceedingNode(&FingerTable, data.Key)
+		value = closest_node.DataTable[data.Key]	
 	}
 
 	return
