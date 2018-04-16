d603fff54b09a1d01e0bbea83e7f642fd71d8379
nallg00d <nick.allgood@gmail.com>
2018-04-16 17:09:07 -0400

    reformatted message and added leave-ring instructions

--- PROJ1/leave_ring/leave_ring.go
+++ PROJ1/leave_ring/leave_ring.go
@@ -1,8 +1,21 @@
 package leave_ring
 
 import "log"
-import "fmt"
 import chord "../utils/node_defs"
+import msg "../utils/message_defs"
+
+// func SendDataToBucket(node_id int64, bucket_data int64){
+
+/*
+type Message struct {
+        Do string `json:"do"`
+        SponsoringNode int64 `json:"sponsoring-node"`
+        Mode string `json:"mode"`
+        RespondTo int64 `json:"respond-to"`
+        TargetId int64 `json:target-id`
+        Data Data `json:"data"`
+}
+*/
 
 func Leave_ring(node *chord.Node, mode string) {
 
@@ -18,8 +31,15 @@ func Leave_ring(node *chord.Node, mode string) {
 		case "orderly":
 			log.Printf("\nNode: %d is leaving orderly\n", node.ChannelId)
 			// stuff to tell other nodes
+
+//			network[node.Successor] <- "Node is leaving"
+//			network[node.Predecessor] <- "Node is leaving"
 			
-			// Loop through nodes fingertable to append to successor
+			// Loop through current nodes DataTable to append to successor
+			for k, v := range node.DataTable {
+
+				var message = msg.Message {Do:"store-data", Mode:"orderly", TargetId:node.ChannelID,  Data:{k:v}}
+			}
 	
 			// remove node from ring
 			node.Predecessor = -1
