ff7e749d973076245dbeae3379d375f3ac89d736
nallg00d <nick.allgood@gmail.com>
2018-04-16 22:14:18 -0400

    Fixing up with FindClosestPreceedingNode

--- PROJ1/get/get.go
+++ PROJ1/get/get.go
@@ -1,40 +1,16 @@
 package get
 
-import "fmt"
 import msg "./utils/message_defs"
 import node "./utils/node_defs"
 
+func Get(data *msg.Data, respond-to int64, node_obj *node.Node)(value string) {
 
-
-
-func Get(key String, respond-to int64)(value string) {
-
-	var found = false
-	var closest_node = nil
 	// Get Node Data
 	var node_id = map_to_string_id(key)
-
-	// Check to see if we have it
-	if ChannelID == node_id {
-		value := DataTable[key]
-	} else {
-	
-		// Check out Finger tables
-		for k, v := range FingerTable {
-			if k == node_id {
-				value = DataTable[node_id]
-				found = true
-				break
-			}
-		}
-	}
 	
 	// It's not in our finger table so we need to find the next highest node
-	// without over shooting
-	if found == false {
-		closest_node = FindClosestPreceedingNode(&FingerTable, data.Key)
-		value = closest_node.DataTable[data.Key]	
-	}
+	var closest_node = FindClosestPreceedingNode(&node_obj.FingerTable, data.Key)
+	value = closest_node.DataTable[data.Key]	
 
 	return
 }
